dcfc6142dbb907b6d0ae52f9467ec0df0db8034c
louisshao <Louis.Shao@logicsolutions.com.cn>
2013-10-08 16:18:11 +0800

    commit index

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/*
++--------------------------------------------------------------------------
+|   Anwsion [#RELEASE_VERSION#]
+|   ========================================
+|   by Anwsion dev team
+|   (c) 2011 - 2012 Anwsion Software
+|   http://www.anwsion.com
+|   ========================================
+|   Support: zhengqiang@gmail.com
+|   
++---------------------------------------------------------------------------
+*/
+
+if (! file_exists(dirname(__FILE__) . '/system/config/database.php') AND ! file_exists(dirname(__FILE__) . '/system/config/install.lock.php'))
+{
+	header('Location: ./install/');
+	exit;
+}
+
+include('system/system.php');
+
+AWS_APP::run();
